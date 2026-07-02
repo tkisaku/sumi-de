6610de7f20bbbee69b01879ae677d06826fb5e3d
T-Kisaku <kisakutakashi0226@gmail.com>
2026-07-03 03:51:34 +0900

    delete

--- modules/river/scripts/editor-copy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Open $EDITOR in a floating foot terminal; copy the result to the clipboard.
-tmp=$(mktemp --suffix=.txt)
-foot -- "${EDITOR:-nano}" "$tmp"
-[ -s "$tmp" ] && wl-copy < "$tmp"
-rm -f "$tmp"
